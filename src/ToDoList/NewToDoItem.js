5ee9b0ee74a3dec06869910e05723dd0533d7b7e
Guy Livni <guylivni@gmail.com>
2021-03-01 15:56:48 +0100

    update styles

--- src/ToDoList/NewToDoItem.js
+++ src/ToDoList/NewToDoItem.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React from "react";
+import "./NewToDoItem.css";
 
 class NewToDoItem extends React.Component {
-  state = { text: '' };
+  state = { text: "" };
 
   handleAdd = (event) => {
     event.preventDefault();
 
-    this.state.text !== '' && this.props.onAdd(this.state.text);
+    this.state.text !== "" && this.props.onAdd(this.state.text);
 
-    this.setState({ text: '' });
-  }
+    this.setState({ text: "" });
+  };
 
   updateText = (event) => {
     event.preventDefault();
     this.setState({ text: event.target.value });
-  }
+  };
 
   render() {
     return (
@@ -26,7 +27,12 @@ class NewToDoItem extends React.Component {
           data-testid="item-input"
         />
 
-        <button type="button" onClick={this.handleAdd} data-testid="add-button">
+        <button
+          className="add-button"
+          type="button"
+          onClick={this.handleAdd}
+          data-testid="add-button"
+        >
           Add To Do
         </button>
       </div>
--- src/ToDoList/ToDoList.js
+++ src/ToDoList/ToDoList.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./ToDoList.css";
 import ToDoItem from "./ToDoItem";
 import NewToDoItem from "./NewToDoItem";
+import { ReactComponent as Logo } from "../babbelLogo.svg";
 
 class ToDoList extends React.Component {
   state = { items: this.props.list.items };
@@ -32,6 +33,7 @@ class ToDoList extends React.Component {
   render() {
     return (
       <div className="list-container" data-testid="list-container">
+        <Logo />
         <h4 className="list-title" data-testid="list-title">
           {this.props.list.title}
         </h4>
