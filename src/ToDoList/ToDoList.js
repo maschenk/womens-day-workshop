9ed55117e13a295d15c564b50f3775558d057021
Guy Livni <guylivni@gmail.com>
2021-03-02 14:34:42 +0100

    remove components

--- src/ToDoList/ToDoList.js
+++ src/ToDoList/ToDoList.js
@@ -1,35 +1,8 @@
 import React from "react";
 import "./ToDoList.css";
-import ToDoItem from "./ToDoItem";
-import NewToDoItem from "./NewToDoItem";
 import { ReactComponent as Logo } from "../babbelLogo.svg";
 
 class ToDoList extends React.Component {
-  state = { items: this.props.list.items };
-
-  addItem = (newItemText) => {
-    const newItem = { itemText: newItemText, checked: false };
-
-    this.setState((state) => {
-      return {
-        items: [...state.items, newItem],
-      };
-    });
-  };
-
-  checkItem = (index) => {
-    const updatedItemList = this.state.items.map((item, i) => {
-      if (i === index) {
-        item.checked = !item.checked;
-      }
-      return item;
-    });
-
-    this.setState({
-      items: updatedItemList,
-    });
-  };
-
   render() {
     return (
       <div className="list-container" data-testid="list-container">
@@ -37,17 +10,7 @@ class ToDoList extends React.Component {
         <h4 className="list-title" data-testid="list-title">
           {this.props.list.title}
         </h4>
-        <ul className="list-list" data-testid="list-list">
-          {this.state.items.map((item, index) => (
-            <ToDoItem
-              key={item.itemText}
-              item={item}
-              onCheck={this.checkItem}
-              index={index}
-            />
-          ))}
-        </ul>
-        <NewToDoItem onAdd={this.addItem} />
+        Here comes the task list
       </div>
     );
   }
